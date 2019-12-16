5bf69a9235bf7643148ab18be9562f5bf8c1d4be
kauly <kauly@hotmail.com>
2019-12-16 20:26:04 -0300

    first

---
+++ pages/index.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Flex } from "reflexbox";
+import Link from "next/link";
+import Head from "next/head";
+
+const PageOne = () => {
+  return (
+    <Flex
+      width="100%"
+      flexDirection="column"
+      justifyContent="center"
+      sx={{
+        article: {
+          textAlign: "left"
+        },
+        h1: {
+          textAlign: "center"
+        }
+      }}
+    >
+      <Head>
+        <title>Page One</title>
+        <meta name="og:description" content="Amid AOC pressure" />
+        <meta name="og:keywords" content="amid,pressure,Pelosi" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>This is page1</h1>
+      <article>
+        Amid AOC pressure, Pelosi suggests impeachment back on the table to
+        address 'grave new chapter of lawlessness'Amid AOC pressure, Pelosi
+        suggests impeachment back on the table to address 'grave new chapter of
+        lawlessness'Amid AOC pressure, Pelosi suggests impeachment back on the
+        table to address 'grave new chapter of lawlessness'Amid AOC pressure,
+        Pelosi suggests impeachment back on the table to address 'grave new
+        chapter of lawlessness'Amid AOC pressure, Pelosi suggests impeachment
+        back on the table to address 'grave new chapter of lawlessness'Amid AOC
+        pressure, Pelosi suggests impeachment back on the table to address
+        'grave new chapter of lawlessness'Amid AOC pressure, Pelosi suggests
+        impeachment back on the table to address 'grave new chapter of
+        lawlessness'Amid AOC pressure, Pelosi suggests impeachment back on the
+        table to address 'grave new chapter of lawlessness'Amid AOC pressure,
+        Pelosi suggests impeachment back on the table to address 'grave new
+        chapter of lawlessness'Amid AOC pressure, Pelosi suggests impeachment
+        back on the table to address 'grave new chapter of lawlessness'Amid AOC
+        pressure, Pelosi suggests impeachment back on the table to address
+        'grave new chapter of lawlessness'Amid AOC pressure, Pelosi suggests
+        impeachment back on the table to address 'grave new chapter of
+        lawlessness'Amid AOC pressure, Pelosi suggests impeachment back on the
+        table to address 'grave new chapter of lawlessness'
+      </article>
+      <Flex
+        flexDirection="column"
+        alignItems="flex-start"
+        width="100%"
+        sx={{
+          a: {
+            textDecoration: "none",
+            color: "black",
+            fontWeight: "bold",
+            marginTop: "15px",
+            ":hover": {
+              textDecoration: "underline"
+            }
+          }
+        }}
+      >
+        <a href="https://google.com" title="Google">
+          Go to Google
+        </a>
+        <a href="https://yahoo.com" title="Yahoo">
+          Go to Yahoo
+        </a>
+        <a href="https://amazon.com" title="Amazon">
+          Go to Amazon
+        </a>
+        <Link href="/inner">
+          <a>Page Two</a>
+        </Link>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PageOne;
---
+++ pages/inner.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Flex } from "reflexbox";
+import Link from "next/link";
+import Head from "next/head";
+
+const PageTwo = () => {
+  return (
+    <Flex
+      width="100%"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        article: {
+          textAlign: "left"
+        },
+        h1: {
+          textAlign: "center"
+        }
+      }}
+    >
+      <Head>
+        <title>Page Two</title>
+        <meta name="og:description" content="This is page2" />
+        <meta name="og:keywords" content="page2, page, is page" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>This is page2</h1>
+      <article>
+        This is page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2This is page2This is page2This is
+        page2This is page2This is page2
+      </article>
+      <Flex
+        flexDirection="column"
+        alignItems="flex-start"
+        width="100%"
+        sx={{
+          a: {
+            textDecoration: "none",
+            color: "black",
+            fontWeight: "bold",
+            marginTop: "15px",
+            ":hover": {
+              textDecoration: "underline"
+            }
+          }
+        }}
+      >
+        <a href="https://facebook.com" title="Facebook">
+          Facebook
+        </a>
+        <a href="https://instagram.com" title="Instagram">
+          Instagram
+        </a>
+        <a href="https://twitter.com" title="Twitter">
+          Twitter
+        </a>
+        <Link href="/">
+          <a>Page One</a>
+        </Link>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PageTwo;
